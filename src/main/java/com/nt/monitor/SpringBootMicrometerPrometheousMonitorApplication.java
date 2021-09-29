62e85ad1944059e9c61e5680efa606b1077d7c14
niranjan <niranjan.ballia@gmail.com>
2021-09-29 20:42:04 +0530

    Editing the README to try out git add/commit

---
+++ src/main/java/com/nt/monitor/SpringBootMicrometerPrometheousMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.nt.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicrometerPrometheousMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicrometerPrometheousMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nt/monitor/SpringBootMicrometerPrometheousMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicrometerPrometheousMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
